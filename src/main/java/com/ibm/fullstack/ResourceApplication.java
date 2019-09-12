f0798e59ce2b4c3589f09e2821bde815d9ffb124
ParisCheng <cmingwh@cn.ibm.com>
2019-09-12 11:44:03 +0800

    Remove security

--- src/main/java/com/ibm/fullstack/ResourceApplication.java
+++ src/main/java/com/ibm/fullstack/ResourceApplication.java
@@ -1,7 +1,5 @@
 package com.ibm.fullstack;
 
-import java.util.UUID;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,25 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 class ResourceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceApplication.class, args);
 	}
 
-	@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = { "x-auth-token", "x-requested-with", "x-xsrf-token" })
+//	@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = { "x-auth-token", "x-requested-with", "x-xsrf-token" })
 	@RequestMapping("/")
-	public Message home() {
-		return new Message("Hello World");
+	public String home() {
+		return "Hello World";
 	}
 
-	class Message {
-		private String id = UUID.randomUUID().toString();
-		private String content;
-
-		public Message(String content) {
-			this.content = content;
-		}
-		// ... getters and setters and default constructor
-	}
 }
