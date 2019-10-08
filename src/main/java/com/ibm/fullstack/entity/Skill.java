38a44780ac33a54cf60608bc1054a24982441d35
ParisCheng <cmingwh@cn.ibm.com>
2019-10-08 19:59:52 +0800

    initial

---
+++ src/main/java/com/ibm/fullstack/entity/Skill.java
@@ -0,0 +1,33 @@
+package com.ibm.fullstack.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "skill", schema = "fullstack")
+public class Skill {
+
+	@Id
+	@GeneratedValue
+	private Long skill_id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "toc")
+	private String toc;
+
+	@Column(name = "prerequeistes")
+	private String prerequeistes;
+
+}
