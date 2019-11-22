c74739c3083bbe904a6badbcf3807d2fb63e3688
ParisCheng <cmingwh@cn.ibm.com>
2019-11-22 10:47:04 +0800

    Update config

--- src/main/java/com/ibm/fullstack/ResourceApplication.java
+++ src/main/java/com/ibm/fullstack/ResourceApplication.java
@@ -2,10 +2,12 @@ package com.ibm.fullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 @CrossOrigin
